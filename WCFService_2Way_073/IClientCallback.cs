707d087936366392d01ce26abf4eac18d6449bdf
AdiatsaPutra <santikosetyo77@gmail.com>
2021-01-16 04:44:51 +0700

    Add project files.

---
+++ WCFService_2Way_073/IClientCallback.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFService_2Way_073
+{
+    interface IClientCallback
+    {
+        interface IClientCallback
+        {
+            [OperationContract(IsOneWay = true)]
+            void pesankirim(string user, string pesan);
+
+        }
+    }
+}
